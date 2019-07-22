38343495c85dd7b3ace416882546daf7e5c8ed54
zl <zl9@meitu.com>
2019-07-22 14:10:53 +0800

    no message

--- VideoAudio_Processing/run_VAP_extract.py
+++ VideoAudio_Processing/run_VAP_extract.py
@@ -15,14 +15,15 @@ import subprocess
 
 # Set the path of input and output files.
 INPUT_VIDEO = "/data2/zl9/freesound/data/5d269132dce27jh0iyamba5287.mp4"
-OUTPUT_FILE = "/data2/zl9/freesound/data/5d269132dce27jh0iyamba5287.wav"
+OUTPUT_FILE = "/data2/zl9/freesound/data/5d269132dce27jh0iyamba5287.mp3"
 
 # Check if the output file exists. If so, delete it.
 if os.path.isfile(OUTPUT_FILE) is True:
     os.remove(OUTPUT_FILE)
 
 # Set the command for processing the input video/audio.
-cmd = "ffmpeg -i " + INPUT_VIDEO + " -ab 160k -ac 2 -ar 44100 -vn " + OUTPUT_FILE
+# cmd = "ffmpeg -i " + INPUT_VIDEO + " -ab 160k -ac 2 -ar 44100 -vn " + OUTPUT_FILE
+cmd = "ffmpeg -i " + INPUT_VIDEO + " -f mp3 -ab 192000 -vn " + OUTPUT_FILE
 
 # Execute the (Terminal) command within Python.
 subprocess.call(cmd, shell=True)
